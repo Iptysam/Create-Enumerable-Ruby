b2052073ecf03f184f80faafa6142244a2fc492c
Ajayi Ridwan <ridwanullahiajayi01@gmail.com>
2023-03-11 10:13:52 +0100

    Update list.rb

--- list.rb
+++ list.rb
@@ -9,9 +9,16 @@ class MyList
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
-  end
+  def each
+    return to_enum(:each) unless block_given?
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    @list
+end
+  
 end
 
 list = MyList.new(1, 2, 3, 4)
